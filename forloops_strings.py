636b13ec3f2f0c84854445d0f76e636dc8db57cf
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-11 11:32:14 +0530

    occurance of substring in string

--- forloops_strings.py
+++ forloops_strings.py
@@ -13,8 +13,32 @@ def ispalindrome(str):
 '''Create a Python program to reverse the order of words in a given sentence
  using a for loop to iterate over characters by index.'''
 
+def reverseOrder(str):
+    revOrder=""
+    for i in range(len(str)-1,-1,-1):
+        revOrder+=str[i]
+    return revOrder
+# print(reverseOrder("apple"))
+
+
+
 '''Develop a Python program to find and print all occurrences of a given 
 substring within a larger string using a for loop to iterate over characters by index.'''
+sub="bob"
+main="hellobob bobmorley"
+def findOccurance(sub,main):
+    # count=0
+    occur=[]
+    n=len(sub)
+    for i in range(0,len(main)-n):
+        print(main[i:i+n])
+        if main[i:i+n]==sub:
+            
+
+            occur.append(main[i:i+n])
+    return occur
+print(findOccurance(sub,main))
+
 
 '''Write a Python program to compress a given string by replacing consecutive 
 repeated characters with a single character followed by the count of repetition 
