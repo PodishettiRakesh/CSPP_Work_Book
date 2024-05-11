de35ab0f308857ba6259643c00a7e6fd92e3c1fa
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-11 09:47:45 +0530

    reverse word

--- slice_indexing.py
+++ slice_indexing.py
@@ -6,3 +6,8 @@ def checkPalindrome(s):
         return True
     return False
 # print(checkPalindrome("uhhgghhyu"))
+
+'''Create a Python program to reverse the order of words in a given sentence using slicing operations.'''
+def reverseOrder(s):
+    return s[::-1]
+# print(reverseOrder("rakesh"))
