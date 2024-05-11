60d094e8093bfc55757d0f435c854813b653e541
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-11 09:48:29 +0530

    count of sub string

--- slice_indexing.py
+++ slice_indexing.py
@@ -11,3 +11,16 @@ def checkPalindrome(s):
 def reverseOrder(s):
     return s[::-1]
 # print(reverseOrder("rakesh"))
+
+'''Develop a Python program to find and print all occurrences of a given substring 
+within a larger string using slicing operations.'''
+
+def occurance(long,sub):
+    # n=len(sub)
+    count=0
+    for i in range(0,len(long)-3):
+        if long[i]+long[i+1]+long[i+2]==sub:
+            count+=1
+    #         print(sub)
+    # print(count)
+# occurance("jaihanuman jai sr
