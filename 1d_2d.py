0a57c46851f542bee95065aa0b078fe9a703c8a1
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-18 09:06:24 +0530

    find max element from list

--- 1d_2d.py
+++ 1d_2d.py
@@ -9,8 +9,8 @@
 # ele=max(max(row) for row in lis)
 # print(ele)
 
-
-
+'''Write a Python program to add two given matrices (2D lists) and print the result.
+'''
 def sum_matrices(mat1,mat2):
         if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
              return "matrices cannot be added"
@@ -40,9 +40,13 @@ mat2 = [
     [6, 5, 4],
     [3, 2, 1]
 ]
+# print(sum_matrices(mat1,mat2))
 
-
-print(sum_matrices(mat1,mat2))
+'''Create a Python program to find the largest element in a given 1D list and print its value.'''
+def findmax(nums):
+    result=max((row) for row in nums)
+    return result
+print(findmax([1,2,5,8,9,24,12]))
 
 
 
