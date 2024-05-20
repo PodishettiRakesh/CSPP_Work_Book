688c44610dfceab70f051d6e3bedbec67e70914b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-20 06:00:15 +0530

    recursive implementation of the binary search

--- 1d_2d.py
+++ 1d_2d.py
@@ -135,3 +135,107 @@ def rotateRight(lst,positions):
     rotated=lst[-positions:]+lst[0:-positions]
     return rotated
 # print(rotateRight([1,2,3,4],2))
+
+# Immutable datatype in python
+'''Immutable Data Types
+Immutable data types are those whose values cannot be changed after they are created. If you need to modify an immutable object, you must create a new object with the new value.
+
+Common immutable data types in Python include:
+
+Integers
+Floats
+Strings
+Tuples'''
+
+'''Common mutable data types in Python include:
+
+Lists
+Dictionaries
+Sets'''
+
+
+# x=5
+# print(x)
+# print(id(x))
+# x=10
+# print(x)
+# print(id(x))
+
+# mutable data types 
+# tup=(1,2,3,4)
+# print(tup.append(7))
+
+# dic={"a":1,"b":2}
+# print(dic)
+
+# dic["c"]=3
+# print(dic)
+
+
+# s="hello"
+# print(s.append("w"))
+
+# st={1,2,3}
+# st.add(4)
+# print(st)
+
+
+# lst=[1,2,3,4,5]
+# ele=lst.pop()
+# print(lst)
+# print(ele)
+
+# def rev(lis):
+#     lis.clear()
+# lis=[1,2,3,4]
+# rev(lis)
+# print(lis)
+
+
+'''find the factorial for the given number'''
+def fun(n):
+    if n==0:
+        return 1
+    else:
+        return n*fun(n-1)
+# print(fun(5))
+
+def fun(n):
+    if n<=1:
+        return n 
+    else:
+        return fun(n-1)+fun(n-2)
+# print(fun(6))
+
+def fun(n):
+    if n>0:
+        print(n)
+        fun(n-1)
+# fun(5)
+
+def fun(n):
+    if n==1:
+        return 1
+    else:
+        return n+fun(n-1)
+# print(fun(5))
+
+def fun(array,low,high,target):
+    # high=len(array-1)
+    # low=0
+    if high>=low:
+        mid=(high+low)//2
+        if array[mid]==target:
+            return array[mid]
+        elif array[mid]>target:
+            
+            return fun(array,low,mid-1,target)
+        else:
+            return fun(array,mid+1,high,target)
+    else:
+        return -1
+arr=[2,3,4,10,40]
+target=11
+# print(fun(arr,0,len(arr)-1,target))
+
+
