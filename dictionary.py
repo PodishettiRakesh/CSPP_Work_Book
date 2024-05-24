78aa14d4a40898abc965394941a8337972115e95
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-24 11:14:45 +0530

    created first dict with list of words and values as their frequencies

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,16 @@
 #     print(key,val)
 # print("ban" in dic)
 
+'''Write a function that takes a list of words as input and returns 
+a dictionary where the keys are the unique words in the list, and the
+values are the frequencies of those words.'''
+
+def create_dict(lst):
+    dic={}
+    for i in lst:
+        if i in dic:
+            dic[i]+=1
+        else:
+            dic[i]=1
+    return dic
+# print(create_dict(["india","america","rakesh","keerthi","keerthi","kavya","india"]))
