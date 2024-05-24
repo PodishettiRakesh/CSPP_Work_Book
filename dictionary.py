0b41d36339ebe9c9a5fccb256825968400f9a024
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-24 11:25:28 +0530

    merged key-value pairs from both dictionaries

--- dictionary.py
+++ dictionary.py
@@ -21,9 +21,9 @@ def create_dict(lst):
     return dic
 # print(create_dict(["india","america","rakesh","keerthi","keerthi","kavya","india"]))
 
+
 '''Create a function that takes a string as input and returns a dictionary where the 
 keys are the unique characters in the string, and the values are the frequencies of those characters.'''
-
 def chars_dict(str):
     dic={}
     for i in str:
@@ -33,3 +33,19 @@ def chars_dict(str):
             dic[i]=1
     return dic
 # print(chars_dict("Keerthana"))
+
+
+'''Write a function that takes two dictionaries as input and returns a new dictionary
+ containing the merged key-value pairs from both dictionaries. If a key exists in both 
+ dictionaries, the value should be the sum of the values from both dictionaries.'''
+
+def merge_dicts(dic1,dic2):
+    for key in dic2:
+        if key not in dic1:
+            dic1[key]=dic2[key]
+        else:
+            dic1[key]=dic1[key]+dic2[key]
+    return dic1
+dic1={'india': 2, 'america': 1, 'rakesh': 1, 'keerthi': 2, 'kavya': 1}
+dic2={'india': 2, 'america': 1, "australia":2, "paris":1}
+# print(merge_dicts(dic1,dic2))
